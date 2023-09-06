8ad069e579cc9bc352119637a409397dd1f5b909
ralph <hwibaski@gmail.com>
2023-09-07 03:59:31 +0900

    initial commit

---
+++ src/test/java/com/yourlink/YourLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
