549ccb52dbca6257e9051f723501a6748da53c2c
ralph <hwibaski@gmail.com>
2023-09-15 16:06:34 +0900

    chore: core 모듈 내 db 종속 의존성 분리 후 db 모듈 추가 생성
    - db 모듈 추가
      - core 모듈에 존재하던 postgresql, h2 database, flyway 의존성 db 모듈로 이동

--- app-api/src/main/java/com/yourink/YourLinkAppApiApplication.java
+++ app-api/src/main/java/com/yourink/YourLinkAppApiApplication.java
@@ -1,7 +1,11 @@
 package com.yourink;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class YourLinkAppApiApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(YourLinkAppApiApplication.class, args);
     }
 }
