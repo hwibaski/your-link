1162f99f224afb762d26db95580590951a6e4310
ralph <hwibaski@gmail.com>
2023-09-16 17:10:54 +0900

    add: LinkController#updateLink 개발
    - LinkController#updateLink API 라우터 개발
    - CustomException 을 위한 RestControllerAdvice 개발

---
+++ app-api/src/main/java/com/yourink/link/controller/dto/UpdateLinkRequest.java
@@ -0,0 +1,11 @@
+package com.yourink.link.controller.dto;
+
+import com.yourink.validator.ValidUrl;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateLinkRequest(
+        @NotNull(message = "아이디를 확인해주세요") Long id,
+        @NotBlank(message = "타이틀을 확인해주세요") String title,
+        @ValidUrl String linkUrl) {
+}
