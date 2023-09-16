3537d935bfc17fee977da6ebdd47dc3219d365ec
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-09-16 12:24:12 +0900

    에러 처리 위한 ErrorCode enum 클래스 생성 (#4)
    - enum ErrorCode 개발
    - ErrorCode 개발에 따른 ApiResponse 클래스 수정
      - code 필드를 추가

--- core/src/main/java/com/yourink/dto/api/ApiResponse.java
+++ core/src/main/java/com/yourink/dto/api/ApiResponse.java
@@ -3,10 +3,9 @@ package com.yourink.dto.api;
 import java.util.HashMap;
 import java.util.Map;
 
-public record ApiResponse<T>(boolean success, String message, T data, Map<String, String> validation) {
-
-    public static ApiResponse<?> createErrorResponse(String message) {
-        return new ApiResponse<>(false, message, null, new HashMap<>());
+public record ApiResponse<T>(boolean success, String code, String message, T data, Map<String, String> validation) {
+    public static ApiResponse<?> createErrorResponse(String message, String code) {
+        return new ApiResponse<>(false, code, message, null, new HashMap<>());
     }
 
     public void addValidation(String field, String message) {
