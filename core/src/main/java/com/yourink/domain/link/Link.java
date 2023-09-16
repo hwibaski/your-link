1862723e9bfb084708929e56e4a9861d84ab3e3b
ralph <hwibaski@gmail.com>
2023-09-16 13:22:33 +0900

    fix: Link 엔티티 내의 thumbnailUrl 삭제
    - thumbnailUrl 필드 삭제에 따른 수정
      - 관련 생성자 및 정적 팩토리 메서드 수정
      - NoArgs 생성자 접근제어자 protected로 생성

--- core/src/main/java/com/yourink/domain/link/Link.java
+++ core/src/main/java/com/yourink/domain/link/Link.java
@@ -2,12 +2,24 @@ package com.yourink.domain.link;
 
 import com.yourink.domain.base.BaseAuditEntity;
 import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Link extends BaseAuditEntity {
     private String title;
 
     private String linkUrl;
 
-    private String thumbnailUrl;
+    public Link(String title, String linkUrl) {
+        this.title = title;
+        this.linkUrl = linkUrl;
+    }
+
+    public static Link create(String title, String linkUrl) {
+        return new Link(title, linkUrl);
+    }
 }
