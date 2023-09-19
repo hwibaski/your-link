4052eb9c8b02d3370cb4297b0a002f07b2a61abb
ralph <hwibaski@gmail.com>
2023-09-19 17:11:46 +0900

    add: member 도메인 생성

---
+++ core/src/main/java/com/yourink/domain/member/Member.java
@@ -0,0 +1,22 @@
+package com.yourink.domain.member;
+
+import com.yourink.domain.base.BaseAuditEntity;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseAuditEntity {
+    private String email;
+
+    private Member(String email) {
+        this.email = email;
+    }
+
+    public static Member create(String email) {
+        return new Member(email);
+    }
+}
