4592aaa962a14a536512868bab89b99ad0b9caa6
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-09-20 13:28:05 +0900

    태그 저장 기능 개발 (#15)
    * add: 태그 기능 위한 도메인 개발
    
    - Tag Entity 개발
    - TagMap Entity 개발
      - Tag, Member, Link를 연결하는 중간 도메인
    
    * add: 테스트 코드 내 누락된 어노테이션 추가
    
    - MemberServiceTest, LinkServiceTest 내 @ActiveProfiles("test") 추가
    
    * add: TagRepository 개발
    
    - TagRepository#findByNameIsIn 개발
    - Tag 도메인의 name 필드 유니크 제약조건 생성
    
    * add: TagSanitizer 개발
    
    - 매개변수로 받은 문자열 리스트에 대해 가공하는 클래스
      - 영문자는 소문자로 변환
      - 문자열의 앞뒤 공백은 트림
    
    * add: ValidTagFilter 개발
    
    - 유효한 태그만 필터링 해주는 기능 개발
    
    * add: NotSavedTagFilter 개발
    
    - 이미 저장된 태그와 저장할 태그를 인수로 받고 저장되지 않은 문자열 리스트를 반환하는 기능 개발
    
    * add: TagService 개발
    
    - 태그 저장 기능 개발

---
+++ app-api/src/test/java/com/yourink/tag/service/TagServiceTest.java
@@ -0,0 +1,139 @@
+package com.yourink.tag.service;
+
+import com.yourink.domain.tag.Tag;
+import com.yourink.repository.tag.TagRepository;
+import com.yourink.tag.filter.NotSavedTagFilter;
+import com.yourink.tag.filter.ValidTagFilter;
+import com.yourink.tag.sanitizer.TagSanitizer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class TagServiceTest {
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private TagRepository tagRepository;
+
+    @SpyBean
+    private NotSavedTagFilter notSavedTagFilter;
+
+
+    @SpyBean
+    private ValidTagFilter validTagFilter;
+
+    @SpyBean
+    private TagSanitizer tagSanitizer;
+
+
+    @AfterEach
+    void tearDown() {
+        tagRepository.deleteAllInBatch();
+    }
+
+    @Nested()
+    @DisplayName("태그 생성 테스트")
+    class CreateTagTest {
+        @Test
+        @DisplayName("태그를 생성한다")
+        void createTag_success() {
+            // given
+            String tag1 = "tag1";
+            String tag2 = "tag2";
+            String tag3 = "tag3";
+            List<String> tags = List.of(tag1, tag2, tag3);
+
+            // when
+            var result = tagService.createTags(tags);
+
+            // then
+            assertThat(result).extracting("name").containsExactly(tag1, tag2, tag3);
+        }
+
+        @Test
+        @DisplayName("이미 존재하는 태그가 있을 경우 새로운 태그를 만들지 않고 기존에 존재하는 태그를 생성하기 위해 TagFilterService.getTagsToSave 메서드가 호출된다.")
+        void createTag_calls_tagFilterService() {
+            // given
+            String tag1 = "tag1";
+            List<String> tagsToSave = List.of(tag1);
+
+            // when
+            tagService.createTags(tagsToSave);
+
+            // then
+            verify(notSavedTagFilter, times(1)).filterNotSavedTag(any(), any());
+        }
+
+        @Test
+        @DisplayName("태그 이름에 포함된 영어 대문자는 모두 소문자로 변환된다.")
+        void createTag_every_english_char_will_convert_to_lowercase() {
+            // given
+            String tag1 = "Tag1";
+            List<String> tags = List.of(tag1);
+
+            // when
+            var result = tagService.createTags(tags);
+
+            // then
+            assertThat(result).extracting("name").containsExactly("tag1");
+        }
+
+        @Test
+        @DisplayName("태그 생성 시 tagValidator.filterValidTags() 메서드가 호출된다.")
+        void createTag_will_call_tagValidator_validatorTagName() {
+            // given
+            String tag1 = "tag1";
+            List<String> tags = List.of(tag1);
+
+            // when
+            tagService.createTags(tags);
+
+            // then
+            verify(validTagFilter, times(1)).filterValidTags(any());
+        }
+
+        @Test
+        @DisplayName("태그 생성 시 tagSanitizer.trimAndToLowerCase() 메서드가 호출된다.")
+        void createTag_will_call_tagValidator_validatorTagName2() {
+            // given
+            String tag1 = "tag1";
+            List<String> tags = List.of(tag1);
+
+            // when
+            tagService.createTags(tags);
+
+            // then
+            verify(tagSanitizer, times(1)).trimAndToLowerCase(any());
+        }
+
+        @Test
+        @DisplayName("20자가 넘는 tag는 생성되지 않는다.")
+        void createTag_tag_length_is_shorter_than_20() {
+            // given
+            String tag1 = "abcdefghijklmnopqrstu";
+            String tag2 = "abcd";
+            List<String> tags = List.of(tag1, tag2);
+
+            // when
+            List<Tag> result = tagService.createTags(tags);
+
+            // then
+            assertThat(result).extracting("name").containsExactly(tag2);
+        }
+    }
+}
