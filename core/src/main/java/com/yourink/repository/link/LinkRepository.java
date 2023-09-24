59b0f78a2633ba1837e7f7782190ce4b809125c6
ralph <hwibaski@gmail.com>
2023-09-24 16:25:36 +0900

    add: 링크의 조회수를 1 업데이트 시키는 레포지토리 메서드 추가

--- core/src/main/java/com/yourink/repository/link/LinkRepository.java
+++ core/src/main/java/com/yourink/repository/link/LinkRepository.java
@@ -1,10 +1,15 @@
 package com.yourink.repository.link;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.yourink.domain.link.Link;
 import com.yourink.repository.PageableRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LinkRepository extends JpaRepository<Link, Long>, PageableRepository {
     Boolean existsByIdLessThan(Long id);
+
+    @Modifying
+    @Query("UPDATE Link l SET l.hitCount = l.hitCount + 1 WHERE l.id = :id")
+    void updateHitCount(Long id);
 }
