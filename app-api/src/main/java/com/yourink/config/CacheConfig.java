ad1e3274420cb97f30dddb277a8eb3c93dcd1ff2
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-09-25 19:44:52 +0900

    저장된 링크 총 개수 구하는 API 로컬 캐시 적용 (#28)
    * chore: api 모듈 로컬 캐시 도입을 위한 의존성 추가
    
    - caffeine 의존성 추가
    - spring-boot-starter-cache 추가
    
    * add: 로컬 캐시 사용을 위한 config 추가
    - CacheConfig 파일 추가
    - 캐시 관리를 위한 CacheType enum 추가
    
    * add: LinkStatReadService#countAll @Cacheable 어노테이션 적용

---
+++ app-api/src/main/java/com/yourink/config/CacheConfig.java
@@ -0,0 +1,35 @@
+package com.yourink.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.yourink.config.chace.CacheType;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
+
+        List<CaffeineCache> caches = Arrays.stream(CacheType.values())
+                                           .map(cacheType ->
+                                                        new CaffeineCache(cacheType.getCacheName(), Caffeine.newBuilder()
+                                                                                                            .expireAfterWrite(cacheType.getExpiredAfterWriteSecond(), java.util.concurrent.TimeUnit.SECONDS)
+                                                                                                            .maximumSize(cacheType.getMaximumSize())
+                                                                                                            .build())
+                                           ).toList();
+        simpleCacheManager.setCaches(caches);
+
+        return simpleCacheManager;
+    }
+}
+
--- app-api/src/main/java/com/yourink/linkStat/service/LinkStatReadService.java
+++ app-api/src/main/java/com/yourink/linkStat/service/LinkStatReadService.java
@@ -3,6 +3,7 @@ package com.yourink.linkStat.service;
 import com.yourink.linkStat.controller.dto.GetLinkCountResponse;
 import com.yourink.repository.linkStat.LinkStatRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +13,7 @@ public class LinkStatReadService {
     private final LinkStatRepository linkStatRepository;
 
     @Transactional(readOnly = true)
+    @Cacheable(cacheNames = "linkCount")
     public GetLinkCountResponse countAll() {
         return new GetLinkCountResponse(linkStatRepository.count());
     }
