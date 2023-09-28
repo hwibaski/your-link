763ccb9cb072f0796ee634cce45931e0d9c81696
ralph <hwibaski@gmail.com>
2023-09-28 21:38:08 +0900

    feat: 멤버 조회 기능 service 내에 개발

--- core/src/main/java/com/yourink/repository/member/MemberRepository.java
+++ core/src/main/java/com/yourink/repository/member/MemberRepository.java
@@ -3,6 +3,10 @@ package com.yourink.repository.member;
 import com.yourink.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
 }
