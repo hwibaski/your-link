e7d3ad1f5fc19e5f69d15583f25775d7a4e1bd13
Hwimin Kim <85930725+hwibaski@users.noreply.github.com>
2023-09-30 04:29:39 +0900

    서버 시작 스트립트 수정 및 github action 배포용 yml 파일 수정 (#46)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ec2-user/app/app-api"
+PROJECT_ROOT="/home/ec2-user/app"
 DOCKER_COMPOSE_FILE="$PROJECT_ROOT/prod.docker-compose.yml"
 
 docker-compose -f $DOCKER_COMPOSE_FILE down && docker-compose -f $DOCKER_COMPOSE_FILE up --build
